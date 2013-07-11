11d57963b743296f84f96b83947516295d761bfe
eliezerb <eliezerbernart@gmail.com>
2013-07-11 13:50:46 -0300

    Adiciona target="_blank" aos links externos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,4 +9,15 @@ $(document).ready( function(){
     $('#mobile-menu').fadeOut();
   });
 
+
+  // Altera links externos para abrirem em uma nova aba
+  // -- Armazena endereco da pagina
+  var dominio = document.location.protocol+'//'+document.location.host;
+  // -- Compara links com o dominio atual, se for um dominio externo adiciona _blank
+  var links = $('a').each(function(index,element) {
+    if(element.href.substr(0,dominio.length) !== dominio) {
+      element.target = '_blank';
+    }
+  });
+
 });
