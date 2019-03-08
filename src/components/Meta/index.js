87b7ffb9705bd13f00f9afa6c1ef18854be74ceb
Daniel Filho <daniel@dnl.pw>
2019-03-09 00:34:20 +0100

    Better visuals and organisation for the sidebar

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -24,94 +24,93 @@ const Header = ({ siteTitle }) => (
             <ZofeLink to="/contato">Contato</ZofeLink>
           </li>
         </ListItems>
+
+        <ListItems classNames="social-list">
+          <li>
+            <ZofeLink
+              to="https://twitter.com/zofepod"
+              className="social-icon no-underline"
+            >
+              <img src={twitter} alt="ZOFE no Twitter" />
+            </ZofeLink>
+          </li>
+        </ListItems>
+
+        <ListItems classNames="footer-feeds-list">
+          <li className="footer-feed-item">
+            <ZofeLink
+              to="https://zofe.com.br/feed/podcast.xml"
+              className="footer-feed-link"
+              title="Podcast RSS"
+            >
+              Podcast RSS
+            </ZofeLink>
+          </li>
+          <li className="footer-feed-item">
+            <ZofeLink
+              to="itpc://zofe.com.br/feed/podcast.xml"
+              className="footer-feed-link"
+              title="Apple Podcast"
+            >
+              Apple Podcast
+            </ZofeLink>
+          </li>
+          <li className="footer-feed-item">
+            <ZofeLink
+              to="https://www.google.com/podcasts?feed=aHR0cHM6Ly96b2ZlLmNvbS5ici9mZWVkL3BvZGNhc3QueG1s"
+              className="footer-feed-link"
+              title="Google Podcast"
+            >
+              Google Podcast
+            </ZofeLink>
+          </li>
+          <li className="footer-feed-item">
+            <ZofeLink
+              to="https://pca.st/dMr0"
+              className="footer-feed-link"
+              title="Pocketcasts"
+            >
+              Pocketcasts
+            </ZofeLink>
+          </li>
+          <li className="footer-feed-item">
+            <ZofeLink
+              to="https://overcast.fm/itunes580335548/zone-of-front-enders-podcast"
+              className="footer-feed-link"
+              title="Overcast"
+            >
+              Overcast
+            </ZofeLink>
+          </li>
+          <li className="footer-feed-item">
+            <ZofeLink
+              to="http://www.stitcher.com/s?fid=250044&refid=stpr"
+              className="footer-feed-link"
+              title="Stitcher"
+            >
+              Stitcher
+            </ZofeLink>
+          </li>
+          <li className="footer-feed-item">
+            <ZofeLink
+              to="https://open.spotify.com/show/13Q4XtzJ1tSZ7trS4dSOf6?si=LOLsMSQFTcOLSG6a7VLPSw"
+              className="footer-feed-link"
+              title="Spotify"
+            >
+              Spotify
+            </ZofeLink>
+          </li>
+          <li className="footer-feed-item">
+            <ZofeLink
+              to="https://zofe.com.br/feed/posts.xml"
+              className="footer-feed-link"
+              title="Posts RSS"
+            >
+              Posts RSS
+            </ZofeLink>
+          </li>
+        </ListItems>
       </nav>
-
-      <ListItems classNames="social-list">
-        <li>
-          <ZofeLink
-            to="https://twitter.com/zofepod"
-            className="social-icon no-underline"
-          >
-            <img src={twitter} alt="ZOFE no Twitter" />
-          </ZofeLink>
-        </li>
-      </ListItems>
-
-      <h5 className="footer-feeds-title">Feeds</h5>
-      <ListItems classNames="footer-feeds-list">
-        <li className="footer-feed-item">
-          <ZofeLink
-            to="https://zofe.com.br/feed/podcast.xml"
-            className="footer-feed-link"
-            title="Podcast"
-          >
-            Podcast
-          </ZofeLink>
-        </li>
-        <li className="footer-feed-item">
-          <ZofeLink
-            to="itpc://zofe.com.br/feed/podcast.xml"
-            className="footer-feed-link"
-            title="Apple Podcast"
-          >
-            Apple Podcast
-          </ZofeLink>
-        </li>
-        <li className="footer-feed-item">
-          <ZofeLink
-            to="https://www.google.com/podcasts?feed=aHR0cHM6Ly96b2ZlLmNvbS5ici9mZWVkL3BvZGNhc3QueG1s"
-            className="footer-feed-link"
-            title="Google Podcast"
-          >
-            Google Podcast
-          </ZofeLink>
-        </li>
-        <li className="footer-feed-item">
-          <ZofeLink
-            to="https://pca.st/dMr0"
-            className="footer-feed-link"
-            title="Pocketcasts"
-          >
-            Pocketcasts
-          </ZofeLink>
-        </li>
-        <li className="footer-feed-item">
-          <ZofeLink
-            to="https://overcast.fm/itunes580335548/zone-of-front-enders-podcast"
-            className="footer-feed-link"
-            title="Overcast"
-          >
-            Overcast
-          </ZofeLink>
-        </li>
-        <li className="footer-feed-item">
-          <ZofeLink
-            to="http://www.stitcher.com/s?fid=250044&refid=stpr"
-            className="footer-feed-link"
-            title="Stitcher"
-          >
-            Stitcher
-          </ZofeLink>
-        </li>
-        <li className="footer-feed-item">
-          <ZofeLink
-            to="https://open.spotify.com/show/13Q4XtzJ1tSZ7trS4dSOf6?si=LOLsMSQFTcOLSG6a7VLPSw"
-            className="footer-feed-link"
-            title="Spotify"
-          >
-            Spotify
-          </ZofeLink>
-        </li>
-        <li className="footer-feed-item">
-          <ZofeLink
-            to="https://zofe.com.br/feed/posts.xml"
-            className="footer-feed-link"
-            title="Posts"
-          >
-            Posts
-          </ZofeLink>
-        </li>
-      </ListItems>
     </div>
   </header>
 )
