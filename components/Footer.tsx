bc7e5550753dacd31c01a606eec9840655091edc
Daniel Filho <daniel@dnl.dev>
2021-02-23 22:34:00 +0100

    chore: fix links on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,43 +21,43 @@ const Footer = () => (
         </Link>
       </li>
       <li className="inline-block mx-4">
-        <Link href="itpc://zofe.com.br/feed/podcast.xml">
-          <a className="footer-feed-link" title="Apple Podcast">
+        <Link href="https://itunes.apple.com/br/podcast/zone-of-front-enders-podcast/id580335548?mt=2&ls=1">
+          <a className="footer-feed-link" target="_blank" rel="noopener noreferrer" title="Apple Podcast">
             Apple Podcast
           </a>
         </Link>
       </li>
       <li className="inline-block mx-4">
-        <Link href="https://www.google.com/podcasts?feed=aHR0cHM6Ly96b2ZlLmNvbS5ici9mZWVkL3BvZGNhc3QueG1s">
-          <a className="footer-feed-link" title="Google Podcast">
+        <Link href="https://podcasts.google.com/feed/aHR0cDovL2ZlZWRzLnNvdW5kY2xvdWQuY29tL3VzZXJzL3NvdW5kY2xvdWQ6dXNlcnM6MjAyODc2NDAvc291bmRzLnJzcw">
+          <a className="footer-feed-link" target="_blank" rel="noopener noreferrer" title="Google Podcast">
             Google Podcast
           </a>
         </Link>
       </li>
       <li className="inline-block mx-4">
         <Link href="https://pca.st/dMr0">
-          <a className="footer-feed-link" title="Pocketcasts">
+          <a className="footer-feed-link" target="_blank" rel="noopener noreferrer" title="Pocketcasts">
             Pocketcasts
           </a>
         </Link>
       </li>
       <li className="inline-block mx-4">
         <Link href="https://overcast.fm/itunes580335548/zone-of-front-enders-podcast">
-          <a className="footer-feed-link" title="Overcast">
+          <a className="footer-feed-link" target="_blank" rel="noopener noreferrer" title="Overcast">
             Overcast
           </a>
         </Link>
       </li>
       <li className="inline-block mx-4">
         <Link href="http://www.stitcher.com/s?fid=250044&refid=stpr">
-          <a className="footer-feed-link" title="Stitcher">
+          <a className="footer-feed-link" target="_blank" rel="noopener noreferrer" title="Stitcher">
             Stitcher
           </a>
         </Link>
       </li>
       <li className="inline-block mx-4">
         <Link href="https://open.spotify.com/show/13Q4XtzJ1tSZ7trS4dSOf6?si=LOLsMSQFTcOLSG6a7VLPSw">
-          <a className="footer-feed-link" title="Spotify">
+          <a className="footer-feed-link" target="_blank" rel="noopener noreferrer" title="Spotify">
             Spotify
           </a>
         </Link>
