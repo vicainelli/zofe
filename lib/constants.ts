b5b1007833f9e8f294ec10f39b3219bc4c72e190
Daniel Filho <danielfilho@users.noreply.github.com>
2021-03-19 18:50:40 +0100

    chore: remove frequency reference from description

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,7 +4,7 @@ export const FEED_NAME = 'ZOFE'
 export const AUTHORS = 'Daniel Filho, Isa Silveira e Atila Fassina'
 
 export const DESCRIPTION =
-  'Um podcast semanal que aborda assuntos relacionados ao desenvolvimento web, trazendo notícias, cobrindo eventos e opinião.'
+  'Um podcast que aborda assuntos relacionados ao desenvolvimento web, trazendo notícias, cobrindo eventos e opinião.'
 
   export const HOME_OG_IMAGE_URL = 'https://placekitten.com/400/300'
 
